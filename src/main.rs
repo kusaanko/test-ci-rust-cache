e43b8bf408b8522d5523de2478d4b476d096dbc4
Kusaanko <39370373+kusaanko@users.noreply.github.com>
2026-05-10 00:27:10 +0900

    依存関係を追加

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
+use serde::Serialize;
+
 fn main() {
     println!("Hello, world!");
     log::info!("This is an info message.");
     log::warn!("This is a warning message.");
+}
+
+#[derive(Serialize)]
+struct MyStruct {
+    field1: String,
+    field2: i32,
 }
